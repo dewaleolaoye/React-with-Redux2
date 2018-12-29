a61c6b4b82628dea7625a39a7801db9678de7fd0
dewaleolaoye <adewaleolaoye21@gamil.com>
2018-12-29 17:17:05 +0100

    duplicating a single component

--- components/src/index.js
+++ components/src/index.js
@@ -19,6 +19,21 @@ const App = () => {
           <div className="text">Nice blog post!</div>
         </div>
       </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Daniel
+          </a>
+          <div className="metadata">
+            <span className="date">Tommorow by 2:00am</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
     </div>
   );
 };
