8ff90d95e8eb56433f2f438783b211818f2d6290
dewaleolaoye <adewaleolaoye21@gamil.com>
2018-12-30 19:06:41 +0100

    Passing multiple props

--- components/src/index.js
+++ components/src/index.js
@@ -6,7 +6,22 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        date="Dec. 30th, 2018"
+        text="Nice blog post"
+      />
+      <CommentDetail
+        author="Jane"
+        date="Jan. 2nd, 2019"
+        text="Miss Kolawole is beautiful"
+      />
+      <CommentDetail
+        author="Dammy"
+        date="Feb. 11th, 2019"
+        text="I am a FullStack Developer"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
