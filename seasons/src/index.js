e8476b9a68963435ad9eda8608e08acd4ba91cc5
dewaleolaoye <adewaleolaoye21@gamil.com>
2018-12-31 12:43:06 +0100

    Handling error

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,21 +4,28 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         // we called setState!!!
         this.setState({ lat: position.coords.latitude });
       },
-      err => {}
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React says we have to define render
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
